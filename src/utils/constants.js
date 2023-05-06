2d3c1af6fc799c26c481f1f85eb688ba43e14aba
Grechishnikov-Zarin Evgeniy <grecha-zarin@yandex.ru>
2023-05-06 20:59:09 +0300

    select

---
+++ src/utils/constants.js
@@ -0,0 +1,7 @@
+export const towers = ["А", "Б"];
+
+export const floors = Array(25).fill(3).map((item, index) => item + index);
+
+export const rooms = Array(10).fill(1).map((item, index) => item + index)
+
+
